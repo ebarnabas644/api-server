5fa06dce94e7445ca3ab7efa8c530e37409a6f32
ebarnabas <ebarnabas@stud.uni-obuda.hu>
2022-07-13 20:01:12 +0200

    dotenv added

--- app/models/db.js
+++ app/models/db.js
@@ -2,11 +2,11 @@ const mysql = require("mysql2");
 const dbConfig = require("../config/db.config.js");
 // Create a connection to the database
 const connection = mysql.createConnection({
-  host: dbConfig.HOST,
-  user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB,
-  port: dbConfig.PORT
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.DBPORT
 });
 // open the MySQL connection
 connection.connect(error => {
