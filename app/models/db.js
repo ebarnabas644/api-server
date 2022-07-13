3307da70f69d94a903251584c0c653a3d2830c71
ebarnabas <ebarnabas@stud.uni-obuda.hu>
2022-07-13 20:40:12 +0200

    env variable naming fixed

--- app/models/db.js
+++ app/models/db.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
   host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  database: process.env.DB,
   port: process.env.DBPORT
 });
 // open the MySQL connection
