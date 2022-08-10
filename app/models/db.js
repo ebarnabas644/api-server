e5b9d908edf93ecd3bf767b13ab9983e371d99ac
ebarnabas <ebarnabas@stud.uni-obuda.hu>
2022-08-10 16:16:11 +0200

    Fixing connection lost on server idle

--- app/models/db.js
+++ app/models/db.js
@@ -1,20 +1,14 @@
 const mysql = require("mysql2");
-const dbConfig = require("../config/db.config.js");
 const fs = require('fs');
 // Create a connection to the database
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: process.env.HOST,
-  user: process.env.USER,
+  user: process.env.DBUSER,
   password: process.env.PASSWORD,
   database: process.env.DB,
   port: process.env.DBPORT,
   ssl: {
     ca: fs.readFileSync('././ca-certificate.crt')
   }
-});
-// open the MySQL connection
-connection.connect(error => {
-  if (error) throw error;
-  console.log("Successfully connected to the database.");
 });
 module.exports = connection;
