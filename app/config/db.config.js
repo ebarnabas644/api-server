26b882690437fcb2eed931ba0668e6d903851216
ebarnabas <ebarnabas@stud.uni-obuda.hu>
2022-08-10 16:15:37 +0200

    Removing unused config file

--- app/config/db.config.js
+++
@@ -1,9 +0,0 @@
-const  config = {
-    USER:  'root', // sql user
-    PASSWORD:  'root', //sql user password
-    HOST:  '127.0.0.1', // if it does not work try- localhost
-    DB:  'gameSite',
-    PORT:  3306
-  }
-  
-  module.exports = config;
